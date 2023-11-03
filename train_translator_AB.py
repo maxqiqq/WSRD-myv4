6d25206604fd2b4120769a872cf292029054d893
maxqiqq <116487542+maxqiqq@users.noreply.github.com>
2023-11-03 23:45:35 +0800

    Update train_translator_AB.py

--- train_translator_AB.py
+++ train_translator_AB.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     parser.add_argument("--fullres", type=int, default=1, help="[0]inference with hxwx3 [1]fullres inference")
 
     parser.add_argument("--n_epochs", type=int, default=300, help="number of epochs of training")
-    parser.add_argument("--resume_epoch", type=int, default=0, help="epoch to resume training")
+    parser.add_argument("--resume_epoch", type=int, default=0, help="epoch to resume training") # ！！！！！！！！！！！！
     parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
 
     parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
@@ -59,34 +59,34 @@ if __name__ == '__main__':
 
     print('CUDA: ', torch.cuda.is_available(), torch.cuda.device_count())
 
-    # os.makedirs(opt.model_dir, exist_ok=True)
-
     criterion_pixelwise = torch.nn.MSELoss()
     pl = PerceptualLossModule()
 
+    decay_step = (opt.n_epochs - opt.decay_epoch) // opt.decay_steps
+    milestones = [me for me in range(opt.decay_epoch, opt.n_epochs, decay_step)]
+    scheduler = MultiStepLR(optimizer_G, milestones=milestones, gamma=0.5)
+
+    Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+    # 如果保存了就注释掉下面！！！！！！
+    translator = torch.nn.DataParallel(DistillNet(num_iblocks=6, num_ops=4))
+    optimizer_G = torch.optim.Adam(translator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+    
+    if os.path.exists("./best_rmse_model/DistillNet_epoch{}.pth"):
+        translator.load_state_dict(torch.load("./best_rmse_model/DistillNet_epoch{}.pth")) # ！！！！！！！！！
+    if os.path.exists("./best_rmse_model/optimizer_epoch{}.pth"):
+        optimizer_G.load_state_dict(torch.load("./best_rmse_model/optimizer_epoch{}.pth")) # ！！！！！！！！
+    
     cuda = torch.cuda.is_available()
     if cuda:
         device = "cuda"
     else:
         device = "cpu"
 
-    # if opt.model_type == 0:
-    #     translator = UNetTranslator(in_channels=3, out_channels=3)
-    #     translator.apply(weights_init_normal)
-    # else:
-    translator = torch.nn.DataParallel(DistillNet(num_iblocks=6, num_ops=4))
-
     if cuda:
         print("USING CUDA FOR MODEL TRAINING")
         translator.cuda()
         criterion_pixelwise.cuda()
-
-    optimizer_G = torch.optim.Adam(translator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-    decay_step = (opt.n_epochs - opt.decay_epoch) // opt.decay_steps
-    milestones = [me for me in range(opt.decay_epoch, opt.n_epochs, decay_step)]
-    scheduler = MultiStepLR(optimizer_G, milestones=milestones, gamma=0.5)
-
-    Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
     train_il = PairedImageSet('./xiaolunwen-WSRD', 'train', size=(opt.img_height, opt.img_width), use_mask=False, aug=False)
     validation_il = PairedImageSet('./xiaolunwen-WSRD', 'validation', size=None, use_mask=False, aug=False)
@@ -313,4 +313,4 @@ if __name__ == '__main__':
                 print("Saving checkpoint for epoch {} and RMSE {}".format(epoch + 1, best_rmse))
                 torch.save(translator.cpu().state_dict(), "./best_rmse_model/DistillNet_epoch{}.pth".format(epoch))
                 torch.save(optimizer_G.state_dict(), "./best_rmse_model/optimizer_epoch{}.pth".format(epoch))
-                wandb.config.update({"best_rmse": best_rmse}, allow_val_change=True)
+            wandb.config.update({"best_rmse": best_rmse}, allow_val_change=True)
